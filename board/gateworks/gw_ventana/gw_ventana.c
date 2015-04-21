5c40c723bb16867e612280a3c0b6dd4bb6e6b355
Tim Harvey <tharvey@gateworks.com>
2015-04-21 07:25:54 -0700

    imx: ventana: default msata/pci mux to pci before PCI enumeration
    PCI enumeration occurs early, before we fully configure our GPIO's. Make
    sure we steer the MSATA/PCI mux to PCI in board_init to ensure PCI is
    selected before enumeration.
    
    Signed-off-by: Tim Harvey <tharvey@gateworks.com>

--- board/gateworks/gw_ventana/gw_ventana.c
+++ board/gateworks/gw_ventana/gw_ventana.c
@@ -1180,8 +1180,6 @@ static void setup_board_gpio(int board)
 	    test_bit(EECONFIG_SATA, info->config)) {
 		gpio_direction_output(GP_MSATA_SEL,
 				      (hwconfig("msata")) ?  1 : 0);
-	} else {
-		gpio_direction_output(GP_MSATA_SEL, 0);
 	}
 
 #if !defined(CONFIG_CMD_PCI)
@@ -1413,6 +1411,9 @@ int board_init(void)
 		/* GW522x Uses GPIO3_IO23 for PCIE_RST# */
 		if (board_type == GW52xx && ventana_info.model[4] == '2')
 			gpio_cfg[board_type].pcie_rst = IMX_GPIO_NR(3, 23);
+
+		/* MSATA Enable */
+		gpio_direction_output(GP_MSATA_SEL, 0);
 	}
 
 	return 0;
