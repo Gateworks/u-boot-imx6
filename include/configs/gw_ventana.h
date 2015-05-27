fb2cdfe76a8c59172e6dacf895e7a2d54bb43bdb
Tim Harvey <tharvey@gateworks.com>
2015-05-27 15:16:44 -0700

    imx: ventana: config: use explicit addr in loadscript
    If we are loading a script to ${loadaddr} then we need to use that address
    explicitly when calling the source command in case user has changed loadaddr
    
    Signed-off-by: Tim Harvey <tharvey@gateworks.com>

--- include/configs/gw_ventana.h
+++ include/configs/gw_ventana.h
@@ -390,7 +390,7 @@
 	"script=6x_bootscript-ventana\0" \
 	"loadscript=" \
 		"if ${fsload} ${loadaddr} ${bootdir}/${script}; then " \
-			"source; " \
+			"source ${loadaddr}; " \
 		"fi\0" \
 	\
 	"uimage=uImage\0" \
